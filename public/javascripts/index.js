8157c552fcd79d071698ab672cbbf37bf3b845e1
sebLopezCot <sclopez2@hotmail.com>
2016-10-23 18:47:40 -0400

    Dynamically create as many graphs as needed

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,6 @@
 (function index(window){
 
-	var graphx = null;
-	var graphy = null;
+	var graphs = [];
 
 	// Init vars
 	var socket = io();
@@ -13,33 +12,60 @@
 	socket.on('measurement', function(measurement){
 		console.log(measurement);
 
-		if(graphx != null && graphy != null){
+		if($("#chartdiv").html() == ""){
+			var quantities = measurement.split(', ')
+				.map(function(el){
+					var mappings = el.replace(' ', '').split(':');
+					mappings[1] = parseFloat(mappings[1]);
+					return mappings;
+				});
+
+			// skip 0 which is timestamp
+			for(i=1; i < quantities.length; i++){
+				$("#chartdiv").append('<h3>'+quantities[i][0]+'</h3>');
+				$("#chartdiv").append('<canvas id="chart'+quantities[i][0]+'" width="500" height="200"/>');
+				$("#chartdiv").append('<br/>')
+
+				var color = "#ffffff";
+				if(quantities[i][0].toLowerCase().includes('x')){
+					color = "#00ff00";
+				} else if (quantities[i][0].toLowerCase().includes('y')){
+					color = "#ff0000";
+				} else if (quantities[i][0].toLowerCase().includes('z')){
+					color = "#0000ff";
+				}
+
+				graphs.push(new Graph2d("chart" + quantities[i][0], color));
+			}
+		}
+
+		if(graphs.length > 0){
 			var data = measurement.split(', ')
 				.map(function(el) {
 					return parseFloat(el.replace(' ', '').split(':')[1]);
 				});
 
-			graphx.loadData(data[1]);
-			graphy.loadData(data[2]);
+			for (i=0; i < graphs.length; i++){
+				graphs[i].loadData(data[i+1]); // since we don't have a graph for time
+			}
 		}
 	});
 
 	var paused = false;
 
 	$(document).ready(function() {
-		graphx = new Graph2d("chartx", "#00ff00");
-		graphy = new Graph2d("charty", "#ff0000");
-
 		$('#pause').click(function(event) {
 			event.preventDefault();
 
 			if (paused) {
-				graphx.start();
-				graphy.start();
+				graphs.forEach(function(graph){
+					graph.start();
+				});
 				$(this).html("Pause");
 			} else {
-				graphx.stop();
-				graphy.stop();
+				graphs.forEach(function(){
+					graph.stop();
+				});
 				$(this).html("Resume");
 			}
 
